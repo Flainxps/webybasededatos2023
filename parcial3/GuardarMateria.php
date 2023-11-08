63240505656d28b3de1807068dc5d8c5d045384c
Alan castro <124410934+Flainxps@users.noreply.github.com>
2023-11-07 20:36:57 -0600

    Add files via upload

---
+++ parcial3/GuardarMateria.php
@@ -0,0 +1,10 @@
+<?php
+
+    include 'conexion.php' ;
+
+    $nombre - $_POST["nombre"];
+    $semestre - $_POST["semestre"];
+    $especialidad - $_POST["especialidad"];
+
+    $sql = "INSERT INTO materias[ "
+    
