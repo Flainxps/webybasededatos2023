5dba8189e98bf1042823c4bfaa0cbde125e81a69
Alan castro <124410934+Flainxps@users.noreply.github.com>
2023-11-14 19:27:22 -0700

    Add files via upload

--- parcial3/ActualizarMateria.php
+++ parcial3/ActualizarMateria.php
@@ -1,12 +1,12 @@
 <?php
 
     include 'conexion.php';
-
+    $id = $_POST["id"];
     $nombre = $_POST["nombre"];
     $semestre = $_POST["semestre"];
     $especialidad = $_POST["especialidad"];
 
-    $sql = "INSERT INTO materias(nombre, semestre, especialidad) VALUES ('".$nombre."',".$semestre.",'".$especialidad."')";
+    $sql = "UPDATE materias SET nombre='".$nombre."', semestre=".$semestre.", especialidad='".$especialidad."' WHERE id=".$id;
 
     if($conexion->query($sql) === TRUE){
         header("Location: ConsultarMateria.php");
@@ -14,7 +14,8 @@
         exit;
     } else {
         echo "<h2>Ocurrió un error</h2> <p>Error: " .$sql . "<br>" . $conexion->error . "</p>";
-        echo "<h3><a href='ConsultarMateria.php'>Regresar a las Materias</a></h3>";
+        echo "<h3><a href='ConsultarMateria.php'>Regresar a materias</a></h3>";
+        $conexion->close();
     }
 
 ?>
